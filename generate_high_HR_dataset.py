95da73f2698135fe430283c0b3800abf2eff4fc8
JBKC <jmbannon97@gmail.com>
2024-09-15 16:14:26 +0100

    adding full data augmentation

--- generate_high_HR_dataset.py
+++ generate_high_HR_dataset.py
@@ -1,15 +1,98 @@
 '''
-Generate dataset with artificially sped up samples to help model recognise high HR cases
+Generate dataset with artificially sped up samples to help model predict high HR cases
+Combine this dataset with the original & noise datasets to form the final dataset
 '''
 
-import pickle
+import torch
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+from sklearn.utils import shuffle
 
 
-def main():
+class GenerateFullDataset(Dataset):
+    def __init__(self, X, y, X_noise, y_noise, ratio_sampling=0.5):
+        '''
+        __init__ is run when instance of class is created
+        :param X:
+        :param y:
+        :param X_noise:
+        :param y_noise:
+        :param ratio_sampling:
+        '''
 
-    return
+        # convert input data into torch
+        self.X_in = torch.from_numpy(X).float()
+        self.y_in = torch.from_numpy(y).float()
+
+        self.X_noise_in = torch.from_numpy(X_noise).float()
+        self.y_noise_in = torch.from_numpy(y_noise).float()
+
+        self.ratio_sampling = ratio_sampling        # % of random samples taken from adversarial dataset
+        self.fs = 32
+
+        self.create_sped()
+        self.setup_clean_indexes()
+        self.combine_datasets()
+        self.shuffle_dataset()
+
+    def __getitem__(self, index):
+        # returns a single window & corresponding label from dataset at given index
+        return self.X_out[index], self.y_out[index]
+
+    def __len__(self):
+        # returns total number of windows
+        return self.X_out.shape[0]
+
+    def create_sped(self):
+        
+        # create artificially sped up signal, by offsetting (for added variation) -> combining -> downsampling
+        self.X_sped = torch.cat([self.X_in[:-4, ...], self.X_in[4:, ...]], dim=1)[:, ::2, :]
+        # align labels
+        self.y_sped = 2 * self.y_in[:-4]
+
+        # only keep windows that have sped up HR < 300bpm
+        mask = self.y_sped.flatten() < 300
+        self.X_sped = self.X_sped[mask]
+        self.y_sped = self.y_sped[mask]
+
+    def setup_clean_indexes(self):
+        # find "clean" PPG windows - ie ones where the dominant frequency closely matches the ground truth label
+
+        # get FFT
+        fft_sped = torch.abs(torch.fft.fft(self.X_sped[:, 0, :]))[:,:128].T
+        # get dominant frequency & convert to Hz
+        freq_dom = torch.argmax(fft_sped, dim=0) * 7.5
+
+        # get indices of windows with "clean" signals
+        self.clean_indexes = torch.where(torch.abs(freq_dom.flatten() - self.y_sped.flatten()) < 10)[0]
+
+    def combine_datasets(self):
+        # combine all 3 datasets to create the final dataset
+
+        # calculate number of random samples to take
+        n_random_samples = int(self.ratio_sampling * self.y_in.size(0))
+        # get indices for adversarial dataset
+        indexes = torch.randperm(self.y_noise_in.size(0))[:n_random_samples]
+
+        # extract random samples from high HR dataset
+        X_highhr = self.X_sped[self.clean_indexes.flatten()]
+        y_highhr = self.y_sped[self.clean_indexes.flatten()]
+
+        # concatenate all datasets together
+        self.X_out = torch.cat([self.X_in, self.X_noise_in[indexes], X_highhr], dim=0)
+        self.y_out = torch.cat([self.y_in, self.y_noise_in[indexes], y_highhr], dim=0)
 
 
+    def shuffle_dataset(self):
+        # shuffle dataset randomly
 
-if __name__ == '__main__':
-    main()
+        perm = torch.randperm(self.X_out.size(0))
+        self.X_out = self.X_out[perm]
+        self.y_out = self.y_out[perm]
+
+    def on_epoch_end(self):
+        # regenerate the random y_noise labels after each epoch
+
+        self.y_noise_in = torch.from_numpy(np.random.uniform(low=20, high=300, size=(self.X_noise_in.shape[0], 1))).float()
+        self.combine_datasets()
+        self.shuffle_dataset()
---
+++ training_run_full_augmentation.py
@@ -0,0 +1,249 @@
+'''
+Main script for training temporal attention model
+Combines original x_BVP PPG Dalia dataset with adversarial examples & high HR examples
+'''
+
+import numpy as np
+import pickle
+from sklearn.utils import shuffle
+import time
+from temporal_attention_model import TemporalAttentionModel
+import torch
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+
+
+def temporal_pairs(dict, sessions):
+    '''
+    Create temporal pairs between adjacent windows for all data
+    :param dict: dictionary of all session data - each session shape (n_windows, n_channels, n_samples)
+    :param sessions: list of session names
+    :return x_all: temporal pairs of each session as a list of length n_sessions - each entry shape (n_windows, 1, 256, 2)
+    :return y_all: ground truth HR labels as a list of length n_sessions
+    :return act_all: activity labels as a list of length n_sessions
+    '''
+
+    x_all = []
+    y_all = []
+    act_all = []
+
+    for s in sessions:
+
+        x = dict[s]['bvp']
+
+        # pair adjacent windows (i, i-1)
+        x_pairs = (np.expand_dims(x[1:,:],axis=-1) , np.expand_dims(x[:-1,:],axis=-1))
+        x_pairs = np.concatenate(x_pairs,axis=-1)
+        # results in concatenated pairs of shape (n_windows, 1, n_samples, 2)
+
+        x_all.append(x_pairs)
+        y_all.append(dict[s]['label'][1:])
+        act_all.append(dict[s]['activity'][1:])
+
+
+    return x_all, y_all, act_all
+
+def train_model(dict, sessions):
+    '''
+    Create Leave One Session Out split and run through model
+    :param dict: dictionary of all session data - each session shape (n_windows, n_channels, n_samples)
+    :param sessions: list of session names
+    '''
+
+    def NLL(dist, y):
+        '''
+        Negative log likelihood loss of observation y, given distribution dist
+        :param dist: predicted Gaussian distribution
+        :param y: ground truth label
+        :return: NLL for each window
+        '''
+
+        return -dist.log_prob(y)
+
+    # initialise model
+    n_epochs = 500
+    patience = 10               # early stopping parameter
+    batch_size = 256            # number of windows to be processed together
+    n_splits = 4
+
+    # create model instance
+    model = TemporalAttentionModel()
+    optimizer = optim.Adam(model.parameters(), lr=5e-4, betas=(0.9, 0.999), eps=1e-08)
+
+    # create temporal pairs of time windows
+    x, y, act = temporal_pairs(dict, sessions)
+
+    # LOSO splits
+    ids = shuffle(list(range(len(sessions))))       # index each session
+    splits = np.array_split(ids, n_splits)
+
+    # Load checkpoint if available
+    checkpoint_path = '/models/best_temporal_attention_model.pth'
+
+    try:
+        checkpoint = torch.load(checkpoint_path)
+
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        epoch = checkpoint['epoch']
+        best_val_loss = checkpoint['best_val_loss']
+        counter = checkpoint['counter']
+        splits = checkpoint['splits'],
+        processed_splits = checkpoint['processed_splits']
+        last_split_idx = checkpoint['last_split_idx']
+        last_session = checkpoint['last_session']
+        last_session_idx = checkpoint['last_session_idx']
+        print(f"Checkpoint found, resuming from Split {last_split_idx + 1}, Session {last_session + 1}")
+
+    except FileNotFoundError:
+        print("No checkpoint found, training from scratch")
+        best_val_loss = float('inf')        # early stopping parameter
+        counter = 0                         # early stopping parameter
+        processed_splits = []               # track each split as they are processed
+        last_split_idx = -1
+        last_session_idx = -1
+        epoch = -1
+
+    start_time = time.time()
+
+    # outer LOSO split for training data
+    for split_idx, split in enumerate(splits):
+
+        # skip already-processed splits
+        if split_idx <= last_split_idx:
+            continue
+
+        # set training data (current split = testing/validation data)
+        train_idxs = np.array([i for i in ids if i not in split])
+        X_train = np.concatenate([x[i] for i in train_idxs], axis=0)
+        y_train = np.concatenate([y[i] for i in train_idxs], axis=0)
+        act_train = np.concatenate([act[i] for i in train_idxs], axis=0)
+
+        # convert to torch tensor
+        X_train = torch.tensor(X_train, dtype=torch.float32)
+        y_train = torch.tensor(y_train, dtype=torch.float32)
+
+        # create TensorDataset and DataLoader for batching
+        train_dataset = TensorDataset(X_train, y_train)
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+        # inner LOSO split for testing & validation data
+        for session_idx, s in enumerate(split):
+
+            # skip already-processed sessions in current split
+            if split_idx == last_split_idx and session_idx <= last_session_idx:
+                continue
+
+            # set current session to test data
+            X_test = x[s]
+            y_test = y[s]
+            act_test = act[s]
+
+            # set validation data (remainder of current split)
+            val_idxs = np.array([j for j in split if j != s])
+            X_val = np.concatenate([x[j] for j in val_idxs], axis=0)
+            y_val = np.concatenate([y[j] for j in val_idxs], axis=0)
+            act_val = np.concatenate([act[j] for j in val_idxs], axis=0)
+
+            # convert to torch tensors
+            X_val = torch.tensor(X_val, dtype=torch.float32)
+            y_val = torch.tensor(y_val, dtype=torch.float32)
+
+            # training loop
+            for epoch in range(epoch +1, n_epochs):
+
+                model.train()
+
+                # create training batches of windows to pass through model
+                for batch_idx, (X_batch, y_batch) in enumerate(train_loader):
+
+                    optimizer.zero_grad()
+
+                    # prep data for model input - shape is (batch_size, n_channels, sequence_length) = (256, 1, 256)
+                    x_cur = X_batch[:,:,:,0]
+                    x_prev = X_batch[:,:,:,-1]
+
+                    # forward pass through model (convolutions + attention + probabilistic)
+                    dist = model(x_cur, x_prev)
+
+                    # calculate training loss on distribution
+                    loss = NLL(dist, y_batch).mean()
+                    loss.backward()
+                    optimizer.step()
+
+                    print(f'Test session: S{s + 1}, Batch: [{batch_idx + 1}/{len(train_loader)}], '
+                          f'Epoch [{epoch + 1}/{n_epochs}], Train Loss: {loss.item():.4f}')
+
+
+                # validation on whole validation set after each epoch
+                model.eval()
+
+                with torch.no_grad():
+                    val_dist = model(X_val[:,:,:,0], X_val[:,:,:,-1])
+                    val_loss = NLL(val_dist, y_val).mean()          # average validation across all windows
+
+                    print(f'Test session: S{s + 1}, Epoch [{epoch + 1}/{n_epochs}], Validation Loss: {val_loss.item():.4f}')
+
+                # early stopping criteria
+                if val_loss < best_val_loss:
+                    best_val_loss = val_loss
+                    counter = 0
+
+                    # save down checkpoint of current best model state
+                    checkpoint = {
+                        'model_state_dict': model.state_dict(),  # model weights
+                        'optimizer_state_dict': optimizer.state_dict(),  # optimizer state
+                        'epoch': epoch,  # save the current epoch
+                        'best_val_loss': best_val_loss,  # the best validation loss
+                        'counter': counter,  # early stopping counter
+                        'splits': splits,  # training splits
+                        'processed_splits': processed_splits,  # track which splits have already been processed
+                        'last_split_idx': split_idx,  # the index of the last split
+                        'last_session': s,  # the last session in the current split
+                        'last_session_idx': session_idx,  # the index of the last session in the current split
+                    }
+                    torch.save(checkpoint, checkpoint_path)
+
+                else:
+                    counter += 1
+                    if counter >= patience:
+                        print("EARLY STOPPING - onto next split")
+                        break
+
+            # test on held-out session after all epochs complete
+            with torch.no_grad():
+                test_dist = model(X_test[:,:,:,0], X_test[:,:,:,-1])
+                test_loss = NLL(test_dist, y_test).mean()
+                print(f'Test session: S{s + 1}, Test Loss: {test_loss.item():.4f}')
+
+        # mark current split as processed
+        processed_splits.append(split_idx)
+        print(f"Split {split_idx + 1} processed.")
+
+    end_time = time.time()
+    print("TRAINING COMPLETE: time ", (end_time - start_time) / 3600, " hours.")
+
+    return
+
+
+
+def main():
+
+    def load_dict(filename='ppg_filt_dict'):
+
+        with open(filename, 'rb') as file:
+            data_dict = pickle.load(file)
+            print(f'Data dictionary loaded from {filename}')
+
+            return data_dict
+
+    sessions = [f'S{i}' for i in range(1, 16)]
+
+    # load dictionary
+    dict = load_dict()
+    train_model(dict, sessions)
+
+
+if __name__ == '__main__':
+    main()
+
