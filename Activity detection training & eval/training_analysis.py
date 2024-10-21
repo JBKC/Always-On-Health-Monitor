3cb649cb073bb4270621879f5367f072845e5b13
JBKC <jmbannon97@gmail.com>
2024-10-21 02:59:53 +0100

    adding model training analysis

---
+++ Activity detection training & eval/training_analysis.py
@@ -0,0 +1,18 @@
+'''
+File containing analysis for during and after training
+'''
+
+
+def plot_weight_dist():
+
+
+
+    return
+
+
+def main():
+
+    return
+
+if __name__ == '__main__':
+    main()
