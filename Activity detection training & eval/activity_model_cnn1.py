169a327a37c8fe701c7dc575c3bd76cfdffbcde8
JBKC <jmbannon97@gmail.com>
2024-10-24 02:02:33 +0100

    architecture adjustments

--- Activity detection training & eval/activity_model_cnn1.py
+++ Activity detection training & eval/activity_model_cnn1.py
@@ -13,13 +13,13 @@ class SingleBlock(nn.Module):
     Single Inception-style block architecture
     '''
 
-    def __init__(self, in_channels, n_filters, kernel_size=[3, 5, 7], pooling_size=2):
+    def __init__(self, n_filters, kernel_size=[3, 5, 7], pooling_size=2):
         super().__init__()
 
         # parallel convolutions: each branch 1x1 followed by 1xn
         self.branch = nn.ModuleList([
             nn.Sequential(
-                nn.Conv1d(in_channels=in_channels, out_channels=n_filters, kernel_size=1, stride=1),
+                nn.Conv1d(in_channels=n_filters, out_channels=n_filters, kernel_size=1, stride=1),
                 nn.BatchNorm1d(n_filters),
                 nn.ELU(),
                 nn.Conv1d(in_channels=n_filters, out_channels=n_filters,
@@ -27,31 +27,26 @@ class SingleBlock(nn.Module):
             for ks in kernel_size
         ])
 
-        self.pooling = nn.MaxPool1d(kernel_size=pooling_size, stride=pooling_size,
-                                    padding=(pooling_size - 1) // 2 if pooling_size % 2 == 0 else pooling_size // 2)
-        self.conv1x1 = nn.Conv1d(in_channels=n_filters, out_channels=in_channels, kernel_size=1)
-        self.bn1 = nn.BatchNorm1d(n_filters)
-        self.bn2 = nn.BatchNorm1d(in_channels)
+        self.pooling = nn.MaxPool1d(kernel_size=pooling_size, stride=pooling_size,padding=0)
+        self.conv1x1 = nn.Conv1d(in_channels=n_filters, out_channels=n_filters, kernel_size=1)
+        self.bn = nn.BatchNorm1d(n_filters*4)
 
     def forward(self, X):
 
         # pass in parallel through each branch
         out = [b(X) for b in self.branch]
-        print(f'Branches shape: {[i.shape for i in out]}')
+        # print(f'Branches shape: {[i.shape for i in out]}')
+        out = torch.cat(out, dim=1)
+        # print(f'All branches shape: {out.shape}')
 
-        # sum up the result
-        out = self.bn1(torch.sum(torch.stack(out), dim=0))
-        # upscale to match input
-        out = self.conv1x1(out)
-        print(out.shape)
+        # pool branch
+        pool_out = self.pooling(X)
+        # print(f'Pool shape: {pool_out.shape}')
 
-        # residual branch
-        res = self.conv1x1(self.pooling(X))
-        print(res.shape)
+        # depth concatenate
+        out = F.elu(self.bn(torch.cat((out, pool_out), dim=1)))
 
-        # add residual
-        out = F.elu(self.bn2(out + res))
-        print(out.shape)
+        # print(f'Multi_kernel out shape: {out.shape}')
 
         return out
 
@@ -61,18 +56,14 @@ class MultiKernel(nn.Module):
     Series of repeating Inception-style blocks
     '''
 
-    def __init__(self, bottleneck, out_channels):
+    def __init__(self, n_filters, n_blocks):
         super().__init__()
-
-        n_blocks = 3
 
         # create stacked structure of Inception blocks
         self.blocks = nn.ModuleList([
-            SingleBlock(in_channels=out_channels, n_filters=bottleneck)
-            for _ in range(n_blocks)
+            SingleBlock(n_filters=n_filters*4**(i))
+            for i in range(n_blocks)
         ])
-
-        self.bn = nn.BatchNorm1d(out_channels)
 
     def forward(self, X):
         # iterate over each block in the ModuleList
@@ -87,22 +78,23 @@ class InitialBlock(nn.Module):
     Input convolutional block, pre-Inception blocks
     '''
 
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, kernel_size=3):
         super().__init__()
 
         pooling_size = 3
 
         self.conv1 = nn.Conv1d(in_channels=in_channels, out_channels=out_channels,
-                               kernel_size=3, stride=2, padding='valid')
+                               kernel_size=kernel_size, stride=2,
+                               padding=(kernel_size - 1) // 2 if kernel_size % 2 == 0 else kernel_size // 2)
 
         self.bn = nn.BatchNorm1d(num_features=out_channels)
-        self.pooling = nn.MaxPool1d(kernel_size=pooling_size, stride=2, padding=0)
+        self.pooling = nn.MaxPool1d(kernel_size=pooling_size, stride=2, padding=1)
 
     def forward(self, X):
-        print(f'Input shape: {X.shape}')
+        # print(f'Input shape: {X.shape}')
         X = self.bn(self.conv1(X))
         X = self.pooling(F.elu(X))
-        print(f'Initial block out shape: {X.shape}')
+        # print(f'Initial_block out shape: {X.shape}')
 
         return X
 
@@ -116,12 +108,14 @@ class AccModel(nn.Module):
     def __init__(self, in_channels, num_classes):
         super().__init__()
 
-        out_channels = 256  # number of feature maps to achieve at the output of each block
+        n_filters = 32                              # number of initial filters
+        n_blocks = 3                                # number of repeating Inception blocks
+        gap_out = n_filters*4**(n_blocks)           # number of channels in GAP output
 
-        self.initial_block = InitialBlock(in_channels=in_channels, out_channels=out_channels)
-        self.multi_kernel = MultiKernel(bottleneck=out_channels//4, out_channels=out_channels)
+        self.initial_block = InitialBlock(in_channels=in_channels, out_channels=n_filters)
+        self.multi_kernel = MultiKernel(n_filters=n_filters, n_blocks=n_blocks)
         self.gap = nn.AdaptiveAvgPool1d(output_size=1)  # global average pooling
-        self.fc = nn.Linear(out_channels, num_classes)
+        self.fc = nn.Linear(gap_out, num_classes)
         self.dropout = nn.Dropout(p=0.2)
 
     def forward(self, X):
@@ -131,6 +125,7 @@ class AccModel(nn.Module):
         X = self.multi_kernel(X)
         # global average pooling
         X = torch.squeeze(self.gap(X), dim=-1)
+        # print(f'Post-GAP shape: {X.shape}')
         # dropout
         X = self.dropout(X)
         # FCN to output
