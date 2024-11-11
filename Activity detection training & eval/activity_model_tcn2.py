de82c9a65332d6edbf83d76b6b1cb9d7689f0613
JBKC <jmbannon97@gmail.com>
2024-11-11 04:57:34 +0000

    adding model architecture

--- Activity detection training & eval/activity_model_tcn2.py
+++ Activity detection training & eval/activity_model_tcn2.py
@@ -7,97 +7,33 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
-class Inception(nn.Module):
-    '''
-    Single Inception block
-    '''
-
-    def __init__(self, in_channels, n_filters, pooling_size, stride, kernel_size=[3,5,7]):
-
+class PWConv(nn.Module):
+    def __init__(self)
         super().__init__()
 
-        # 1x1 bottleneck
-        self.conv1x1 = nn.Conv1d(in_channels=in_channels,out_channels=n_filters,
-                                     kernel_size=1,stride=stride)
+    def forward(self):
+        return
 
-        # parallel convolutions
-        self.branches = nn.ModuleList([nn.Conv1d(
-            in_channels=n_filters,out_channels=n_filters,
-            kernel_size=ks,stride=stride,padding=(ks-1)//2 if ks % 2 == 0 else ks//2) for ks in kernel_size])
-
-        # pooling branch
-        self.pooling = nn.MaxPool1d(kernel_size=pooling_size, stride=stride,
-                                    padding=(pooling_size-1)//2 if pooling_size % 2 == 0 else pooling_size//2)
-
-        self.bn = nn.BatchNorm1d(n_filters)
-        self.dropout = nn.Dropout(p=0.2)
-
-    def forward(self, X):
-
-        # pass through 1x1 bottleneck
-        X1x1 = F.relu(self.bn(self.conv1x1(X)))
-
-        # pass through convolution branches in parallel to get list
-        conv_out = [self.bn(branch(X1x1)) for branch in self.branches]
-
-        # pass original input through maxpool branch
-        max_out = self.bn(self.conv1x1(self.pooling(X)))
-        conv_out.append(max_out)
-
-        # concatenate outputs across channel dimension
-        out = F.relu(torch.cat(conv_out, dim=1))
-        # out = self.dropout(out)
-
-        return out
-
-
-class InceptionBlocks(nn.Module):
-    '''
-    Series of repeating Inception blocks
-    '''
-
-    def __init__(self, in_channels, n_filters):
-
+class ConvFF(nn.Module):
+    def __init__(self)
         super().__init__()
 
-        n_blocks = 6
-        pooling_size = 3
-        n_out = n_filters * 4          # channel dimension after concatenating 4 branches at end of each block
+    def forward(self):
+        return
 
-        # create stacked structure of Inception blocks
-        self.conv_blocks = nn.ModuleList([
-            Inception(in_channels=in_channels if i == 0 else n_out, n_filters=n_filters,
-                      pooling_size=pooling_size, stride=1)
-            for i in range(n_blocks)
-        ])
+class DWConv(nn.Module):
+    def __init__(self)
+        super().__init__()
 
-        self.res1x1 = nn.Conv1d(in_channels=in_channels,out_channels=n_out,
-                                     kernel_size=1,stride=1)
+    def forward(self):
+        return
 
-        self.bn = nn.BatchNorm1d(n_out)
+class Patching(nn.Module):
+    def __init__(self)
+        super().__init__()
 
-
-    def forward(self, X):
-
-        # save input for residual connection
-        residual = X
-
-        # pass through each Inception block in turn
-        for i, block in enumerate(self.conv_blocks):
-            X = block(X)
-
-            # add residual every 3rd connection
-            if (i + 1) % 3 == 0:
-                if residual.shape != X.shape:
-                    # match shapes
-                    residual = self.bn(self.res1x1(residual))
-
-                X = F.relu(self.bn(X + residual))
-                residual = X
-
-        return X
-
+    def forward(self):
+        return
 
 class AccModel(nn.Module):
     '''
@@ -107,26 +43,20 @@ class AccModel(nn.Module):
     def __init__(self, in_channels, num_classes):
         super().__init__()
 
-        n_filters = 8               # number of filters to use throughout model
-
-        self.inception_blocks = InceptionBlocks(in_channels, n_filters)
-        self.gap = nn.AdaptiveAvgPool1d(output_size=1)  # global average pooling
-
-        self.fc = nn.Linear(32, num_classes)
-        self.dropout = nn.Dropout(p=0.2)
-
+        self.patching = Patching()
+        self.dw_conv = DWConv()
+        self.conv_ff = ConvFF()
+        self.bn = nn.BatchNorm1d()
+        self.head = nn.Linear()
 
     def forward(self, X):
 
-        # pass through InceptionTime blocks
-        X = self.inception_blocks(X)
+        res = self.patching(X)          # res = input into proxy-transformer architecture
 
-        # perform global average pooling
-        X = torch.squeeze(self.gap(X), dim=-1)
-        X = self.dropout(X)
-
-        # FCN to output
-        X = self.fc(X)
+        X = self.bn(self.dw_conv(res))
+        X = res + self.conv_ff(self.conv_ff(X))
+        ## flatten
+        out = self.head(X)
 
         return X
 
