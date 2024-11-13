1fb487acad4975014efb4e67898057b5cafc4004
JBKC <jmbannon97@gmail.com>
2024-11-13 03:32:46 +0000

    constructing model architecture

--- Activity detection training & eval/activity_model_tcn2.py
+++ Activity detection training & eval/activity_model_tcn2.py
@@ -36,8 +36,8 @@ class ConvFFN2(nn.Module):
         super().__init__()
 
         r = 1
-        self.pw21 = nn.Conv1d(in_channels=M * D, out_channels=r * M * D, kernel_size=1, stride=1, groups=D)
-        self.pw22 = nn.Conv1d(in_channels=r * M * D, out_channels=M * D, kernel_size=1, stride=1, groups=D)
+        self.pw21 = nn.Conv1d(in_channels=D * M, out_channels=r * D * M, kernel_size=1, stride=1, groups=D)
+        self.pw22 = nn.Conv1d(in_channels=r * D * M, out_channels=D * M, kernel_size=1, stride=1, groups=D)
 
     def forward(self, z):
 
@@ -51,26 +51,26 @@ class DWConv(nn.Module):
     '''
     input shape = (B,M*D,N)
     '''
-    def __init__(self, C, klarge=51, ksmall=5, stride=1):
+    def __init__(self, M, D, klarge=51, ksmall=5, stride=1):
         super().__init__()
 
-        self.dw_large = nn.Conv1d(in_channels=C,out_channels=C,kernel_size=klarge,stride=stride,
-                                    padding=klarge//2,groups=C)
-        self.dw_small = nn.Conv1d(in_channels=C,out_channels=C,kernel_size=ksmall,stride=stride,
-                                    padding=ksmall//2,groups=C)
+        self.dw_large = nn.Conv1d(in_channels=M*D,out_channels=M*D,kernel_size=klarge,stride=stride,
+                                    padding=klarge//2,groups=M*D)
+        self.dw_small = nn.Conv1d(in_channels=M*D,out_channels=M*D,kernel_size=ksmall,stride=stride,
+                                    padding=ksmall//2,groups=M*D)
 
-        self.bn = nn.BatchNorm1d(C)
+        self.bn = nn.BatchNorm1d(M*D)
 
     def forward(self, z):
 
-        zl = self.bn(self.dw_large(z))
+        zl = self.dw_large(z)
         zs = self.bn(self.dw_small(z))
 
         return zl+zs
 
 class Patching(nn.Module):
     '''
-    input shape = (B,M,1,L)
+    input shape = (B*M,1,L)
     '''
     def __init__(self, n_embd, patch_size=1, stride=1):
         super().__init__()
@@ -80,8 +80,6 @@ class Patching(nn.Module):
 
     def forward(self, x):
 
-        # reshape for Pytorch convolution (B*M,1,L)
-        x = x.reshape(-1, 1, x.size(-1))
         x = self.patch_conv(x)
 
         return x
@@ -97,10 +95,10 @@ class AccModel(nn.Module):
         dmin = 32
         dmax = 512
         n_embd = min(max(2 ** np.log(in_channels), dmin), dmax)     # embedding dimension (D)
-        C = in_channels * n_embd                                    # C = M*D
 
         self.patching = Patching(n_embd)
-        self.dw = DWConv(C)
+        self.dw = DWConv(M=in_channels,D=n_embd)
+        self.bn = nn.BatchNorm1d(in_channels*n_embd)
         self.ff1 = ConvFFN1(M=in_channels,D=n_embd)
         self.ff2 = ConvFFN2(M=in_channels, D=n_embd)
         # self.head = nn.Linear()
@@ -111,22 +109,40 @@ class AccModel(nn.Module):
 
         # reshape for patching
         x = x.unsqueeze(2)
+        x = x.reshape(B*M, 1, L)
         x = self.patching(x)
-        res = x.reshape(B, M, x.size(1), x.size(-1))       # residual term
+
+        # reshape for backbone input
+        x = x.reshape(B, M, x.size(1), x.size(-1))       # acts as input into model
+        B,M,D,N = x.shape
 
         # reshape for DWConv
-        z = x.reshape(B, M*x.size(1), x.size(-1))
+        z = x.reshape(B,M*D,N)
         z = self.dw(z)
+
+        # apply batchnorm
+        z = z.reshape(B,M,D,N)
+        z = z.reshape(B*M,D,N)
+        z = self.bn(z)
+        z = z.reshape(B,M,D,N)
+        z = z.reshape(B,M*D,N)
 
         # ConvFFN1
         z = self.ff1(z)
 
-        # reshape for ConvFFN2
-        z = x.reshape(B, M, res.size(-2), res.size(-1))
+        # reshape & permute for ConvFFN2
+        z = z.reshape(B, M, D, N)
         z = z.permute(0,2,1,3)
-        z = z.reshape(B, z.size(1)*M, z.size(-1))
-        z = (self.ff2(z))
+        z = z.reshape(B, D*M, N)
+        z = self.ff2(z)
 
+        # reshape & permute to match input
+        z = z.reshape(B, D, M, N)
+        z = z.permute(0,2,1,3)
+        out = x + z
+        print(out.shape)
+
+        # pass into
 
 
         # x = res + self.conv_ff(self.conv_ff(x))
