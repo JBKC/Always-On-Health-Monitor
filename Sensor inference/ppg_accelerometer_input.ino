aafd65e923c4b17a1358b8178bbefd53306720fa
James Bannon <121674801+JBKC@users.noreply.github.com>
2024-11-29 19:20:51 +0000

    Update ppg_accelerometer_input.ino

--- Sensor inference/ppg_accelerometer_input.ino
+++ Sensor inference/ppg_accelerometer_input.ino
@@ -8,6 +8,10 @@ Adafruit_MMA8451 mma = Adafruit_MMA8451();
 // convert from m/s**2 to g
 float conv = 9.80665;
 
+// set interval to ensure sampling rate of 32
+unsigned long lastTime = 0;
+const unsigned long interval = 1000 / 32;
+
 void setup() {
     Serial.begin(115200);
 
@@ -20,25 +24,28 @@ void setup() {
 }
 
 void loop() {
+
+    if (millis() - lastTime >= interval) {
+      lastTime = millis();
  
-    // get ppg data directly from pin
-    float ppg = analogRead(A0);
-    Serial.println(ppg, 2);     // 2 decimal places
+      // get ppg data directly from pin
+      float ppg = analogRead(A0);
+      Serial.println(ppg, 2);     // 2 decimal places
 
-    // accelerometer data
-    sensors_event_t event;
-    mma.getEvent(&event);
-    
-    float ax = event.acceleration.x / conv;
-    float ay = event.acceleration.y / conv;
-    float az = event.acceleration.z / conv;
+      // accelerometer data
+      sensors_event_t event;
+      mma.getEvent(&event);
+      
+      float ax = event.acceleration.x / conv;
+      float ay = event.acceleration.y / conv;
+      float az = event.acceleration.z / conv;
 
-    // Send the data in the requested format: ax, ay, az
-    Serial.print(ax,2);
-    Serial.print(",");
-    Serial.print(ay,2);
-    Serial.print(",");
-    Serial.println(az,2);
-
+      // Send the data in the requested format: ax, ay, az
+      Serial.print(ax,2);
+      Serial.print(",");
+      Serial.print(ay,2);
+      Serial.print(",");
+      Serial.println(az,2);
+    }
 
 }
