667a0af50489833e7a13d60d79c84f739db124f1
JBKC <jmbannon97@gmail.com>
2024-11-29 19:07:13 +0000

    adding realtime sensor inference

--- Sensor inference/PPG_realtime_inference.py
+++ Sensor inference/PPG_realtime_inference.py
@@ -1,20 +1,55 @@
 '''
 Passes PPG sensor & accelerometer input through trained model to produce real-time heart-rate inference
+Sensors:
+- ppg = DFRobot_Heartrate: fs=
+- accelerometer = Adafruit_MMA8451: fs=
 '''
 
 import torch
+import serial
 from Heartrate_training_eval.temporal_attention_model import TemporalAttentionModel
+import time
+
+
+def clean_stream():
+
+    # pass through ma removal etc
+
+    return
 
 
 
 def main():
 
-    # initialise model
-    checkpoint = torch.load('../models/temporal_attention_model_full_augment_session_S6.pth')
-    model = TemporalAttentionModel()  # Update with any required parameters for your model initialization
+    def downsample():
+        return
 
-    model.load_state_dict(checkpoint['model_state_dict'])
-    model.eval()
+    serial_port = '/dev/cu.usbmodem14101'  # Replace with your Arduino's port (e.g., 'COM3' on Windows)
+    baud_rate = 115200
+
+    try:
+        ser = serial.Serial(serial_port, baud_rate, timeout=1)
+        print(f"Connected to {serial_port} at {baud_rate} baud")
+        time.sleep(2)  # Wait for Arduino to reset
+    except serial.SerialException as e:
+        print(f"Error: {e}")
+        exit()
+
+    while True:
+        if ser.in_waiting > 0:  # Check if data is available
+            data = ser.readline().decode('utf-8').strip()  # Read and decode the data
+            print(data)
+
+
+
+
+    #
+    # # initialise model
+    # checkpoint = torch.load('../models/temporal_attention_model_full_augment_session_S6.pth')
+    # model = TemporalAttentionModel()  # Update with any required parameters for your model initialization
+    #
+    # model.load_state_dict(checkpoint['model_state_dict'])
+    # model.eval()
 
 if __name__ == '__main__':
     main()
