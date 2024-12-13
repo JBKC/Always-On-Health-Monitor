3628b7453adc8bfdd971aefbc4de9aed6da476b0
James Bannon <121674801+JBKC@users.noreply.github.com>
2024-12-13 07:46:02 +0000

    Update realtime_activity_eval.py

--- Sensor inference/realtime_activity_eval.py
+++ Sensor inference/realtime_activity_eval.py
@@ -1,1 +1,30 @@
+'''
+Called from ESP32_BT_realtime_activity_inference.py script
+'''
 
+import numpy as np
+import torch
+
+def run_inference(x, model):
+
+    model.eval()
+    x = torch.from_numpy(x).float()
+
+    out = model(x)
+    softmax = torch.nn.Softmax(dim=1)
+    out = softmax(out)
+
+    return int(torch.argmax(out, dim=1))
+
+
+def main(x, model):
+    '''
+    :param x: shape (1,6,256)
+    :param model: time series model
+    '''
+
+    return run_inference(x, model)
+
+
+if __name__ == '__main__':
+    main()
