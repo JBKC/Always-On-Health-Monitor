f4b7b1eb386b3f155fd99207d2e7c581ea3fcf71
James Bannon <121674801+JBKC@users.noreply.github.com>
2024-12-23 20:01:21 +0000

    Update realtime_activity_processing.py

--- Sensor inference/realtime_activity_processing.py
+++ Sensor inference/realtime_activity_processing.py
@@ -8,7 +8,7 @@ from scipy.signal import butter, filtfilt
 def butter_filter(signal, btype, lowcut=None, highcut=None, fs=32, order=5):
     """
     Applies Butterworth filter
-    :param signal: input signal of shape (n_samples,)
+    :param signal: input signal of shape (n_channels, n_samples)
     :return smoothed: smoothed signal of shape (n_channels, n_samples)
     """
 
@@ -26,13 +26,15 @@ def butter_filter(signal, btype, lowcut=None, highcut=None, fs=32, order=5):
         b, a = butter(order, low, btype=btype)
 
     # apply filter using filtfilt (zero-phase filtering)
-    filtered = np.array(filtfilt(b, a, signal))
+    # filtered = np.array(filtfilt(b, a, signal))
+
+    filtered = np.array([filtfilt(b, a, channel) for channel in signal])
 
     return filtered
 
 def z_normalise(X):
     '''
-    Z-normalises data for all windows, across each channel, using vectorisation
+    Z-normalises data for a single window, across each channel, using vectorisation
     :param X: of shape (1, 6, 256)
     :return: X_norm: of shape (1, 6, 256)
     '''
@@ -48,24 +50,31 @@ def z_normalise(X):
     return (X - ms_reshaped) / np.where(stds_reshaped != 0, stds_reshaped, 1)
 
 
-def main(snapshot):
+def main(snapshot, multi=None):
     '''
     Accepts 1 window of shape (256,4)
-    Returns shape (1,6,256)
+    Returns shape (1,6,256) = (n_windows, n_channels, n_samples)
+    "multi" is a flag to determine if snapshot needs extra pre-processing
     '''
 
-    # filter ppg data
+    # check for if multiple windows are inputted
+    if multi==1:
+        snapshot = snapshot[-256:, :]
 
-    ppg = snapshot[:,0]
+    # transform into correct shape + filter
+    ppg = np.expand_dims(snapshot[:,0].T,axis=0)
+    acc = snapshot[:, 1:].T
 
     c = butter_filter(signal=ppg, btype='bandpass', lowcut=0.5, highcut=4)             # cardiac
     r = butter_filter(signal=ppg, btype='bandpass', lowcut=0.2, highcut=0.35)          # respiratory
     m = butter_filter(signal=ppg, btype='highpass', lowcut=4)                          # motion artifacts
 
-    ppg = np.stack([c,r,m])
-    out = np.concatenate([ppg, snapshot[:, 1:].T], axis=0)
+    acc = butter_filter(signal=acc, btype='lowpass', highcut=10)
 
+    # combine ppg and acc
+    out = np.concatenate([c, r, m, acc], axis=0)
     out = np.expand_dims(out, axis=0)
+    # print(out.shape)
 
     # z-normalise
     out = z_normalise(out)
